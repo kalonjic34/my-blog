bed1d7ff563c1fc320dbd0cc981bbca0572b65c5
Christian <kalonjic34@gmail.com>
2026-08-13 19:25:45 +0200

    add initial blog post index and show pages

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog Site</title>
+</head>
+<body>
+    <h1>My personal blog</h1>
+    <ul>
+            <li>
+        <a href="/post/1">Post One</a>
+    </li>
+    <li>
+        <a href="/post/2">Post Two</a>
+    </li>
+    </ul>
+
+</body>
+</html>
