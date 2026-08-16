f9aab8d35e454148742d4c101a71178851b1eabd
Christian <kalonjic34@gmail.com>
2026-08-16 13:29:37 +0200

    fix: correct variable name for posts in index view

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Spatie\Sheets\Facades\Sheets;
 
 Route::get('/', function () {
-    $post = Sheets::collection('posts')->all();
-    return view('posts.index', ['post'=> $post]);
+    $posts = Sheets::collection('posts')->all();
+
+    return view('posts.index', [
+        'posts' => $posts,
+    ]);
 });
 
-
-Route::get('/post/{post}', function ($post) {
-
-    return view('posts.show', ['post'=>$post]);
+Route::get('/posts/{post}', function ($post) {
+    return view('posts.show', ['post' => $post]);
 });
