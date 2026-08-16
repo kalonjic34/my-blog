e3aa98afa6dd728232726fbbb6eef982d31fc93c
Christian <kalonjic34@gmail.com>
2026-08-16 18:19:25 +0200

    enhance post display in index view with title, excerpt, and author details

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,9 @@
     <ul>
         @foreach ($posts as $post)
             <li>
-        <a href="/posts/{{$post ->slug}}">{{ $post -> slug}}</a>
+        <h2><a href="/posts/{{$post ->slug}}">{{ $post -> title}}</a></h2>
+        <div>{{ $post->excerpt }}</div>
+        <div>{{ $post->author }} / {{ $post->date->diffForHumans() }}</div>
     </li>
     @endforeach
     </ul>
