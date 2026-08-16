121be5f89c6f5071a7f48bccedc1da7de3df302d
Christian <kalonjic34@gmail.com>
2026-08-16 13:28:53 +0200

    fix: update filesystem configuration for local and posts disks

--- config/filesystems.php
+++ config/filesystems.php
@@ -31,11 +31,15 @@ return [
     'disks' => [
 
         'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'private',
             'throw' => false,
         ],
 
         
-        'post' => [
+        'posts' => [
             'driver' => 'local',
             'root' => storage_path('app/posts'),
             'serve' => true,
@@ -52,18 +56,18 @@ return [
             'report' => false,
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-            'report' => false,
-        ],
+        // 's3' => [
+        //     'driver' => 's3',
+        //     'key' => env('AWS_ACCESS_KEY_ID'),
+        //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        //     'region' => env('AWS_DEFAULT_REGION'),
+        //     'bucket' => env('AWS_BUCKET'),
+        //     'url' => env('AWS_URL', null),
+        //     'endpoint' => env('AWS_ENDPOINT', null),
+        //     'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        //     'throw' => false,
+        //     'report' => false,
+        // ],
 
     ],
 
